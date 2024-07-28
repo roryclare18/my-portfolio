d586cf7331ca09103285a356917db19506071679
roryclare18 <roryclare18@gmail.com>
2024-07-29 00:52:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
